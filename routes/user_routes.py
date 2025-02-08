9f51fbf25bdf8249554b8928fa208e6622551fb2
vitalii <bondarfamily123@gmail.com>
2025-02-08 15:44:32 +0200

    refactor: Get rid of Primitive Obsession and Long param. list

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -6,25 +6,32 @@ from services.forms import DepositForm, EditPasswordForm, MembershipForm, Paymen
 from services.data_obtainer import get_all_coaches_to_pick, get_all_available_memberships, get_all_coaches
 
 
+class SortParameters:
+    DEFAULT_SORT_COLUMN = 'dateofpayment'
+    DEFAULT_SORT_ORDER = 'DESC'
+
+
 def home_route(db):
     user_service = UserService(db)
-    deposit_from = DepositForm()
+    deposit_form = DepositForm()
     password_form = EditPasswordForm()
 
-    if deposit_from.validate_on_submit():
-        user_service.process_deposit(deposit_from.amount.data, deposit_from.payment_type.data, current_user.member_id)
+    if deposit_form.validate_on_submit():
+        user_service.process_deposit(deposit_form.amount.data, deposit_form.payment_type.data, current_user.member_id)
         flash('Поповнення успішне!', 'success')
         return redirect(url_for('home'))
 
     if password_form.validate_on_submit():
-        if not user_service.change_password(current_user.member_id, password_form.old_password.data, password_form.new_password.data, current_user.password):
+        if not user_service.change_password(current_user.member_id, password_form.old_password.data,
+                                            password_form.new_password.data, current_user.password):
             flash('Неправильний старий пароль', 'danger')
             return redirect(url_for('home'))
 
         flash('Ваш пароль успішно змінено', 'success')
         return redirect(url_for('home'))
 
-    return render_template('home.html', current_user=current_user, deposit_from=deposit_from, password_form=password_form)
+    return render_template('home.html', current_user=current_user, deposit_form=deposit_form,
+                           password_form=password_form)
 
 
 def memberships_route(db):
@@ -35,10 +42,10 @@ def memberships_route(db):
 
     if request.method == 'POST':
         membership_id = request.form.get('membership_id')
-        membership_ids = set([membership['membershipid'] for membership in memberships])
+        membership_ids = {membership['membershipid'] for membership in memberships}
 
         if membership_id in membership_ids:
-            membership = next(membership for membership in memberships if membership['membershipid'] == membership_id)
+            membership = next(m for m in memberships if m['membershipid'] == membership_id)
 
             if membership['price'] > current_user.balance:
                 flash('Недостатньо коштів на рахунку', 'danger')
@@ -47,7 +54,8 @@ def memberships_route(db):
                 flash('Абонемент успішно отриманий!', 'success')
                 return redirect(url_for('memberships'))
 
-    return render_template('memberships.html', memberships=memberships, form=form, membership_id=str(current_user.membership_id))
+    return render_template('memberships.html', memberships=memberships, form=form,
+                           membership_id=str(current_user.membership_id))
 
 
 def coaches_route(db):
@@ -58,34 +66,57 @@ def coaches_route(db):
 def payments_route(db):
     user_service = UserService(db)
     form = PaymentsForm()
-    sort_column = 'dateofpayment'
-    sort_order = 'DESC'
+    sort_column, sort_order = parse_sort_option(form)
+    page, per_page, offset = get_pagination_details()
+    total_pages = count_total_pages(user_service, current_user.member_id, per_page)
 
+    query_params = {
+        'user_service': user_service,
+        'member_id': current_user.member_id,
+        'sort_column': sort_column,
+        'sort_order': sort_order,
+        'limit': per_page,
+        'offset': offset
+    }
+
+    payments = get_sorted_payments(query_params)
+
+    return render_template('payments.html', form=form, payments=payments, current_page=page, total_pages=total_pages)
+
+
+def parse_sort_option(form):
     if form.validate_on_submit():
         sort_option = form.sort_type.data
-        sort_column, sort_order = sort_option.split('_')
+        return sort_option.split('_')
+    return SortParameters.DEFAULT_SORT_COLUMN, SortParameters.DEFAULT_SORT_ORDER
 
+
+def get_pagination_details():
     page = request.args.get('page', 1, type=int)
     per_page = 10
     offset = (page - 1) * per_page
+    return page, per_page, offset
 
+
+def count_total_pages(user_service, member_id, per_page):
     total_payments = user_service.execute_db_query(
         "SELECT COUNT(*) FROM Payments WHERE memberId = :member_id",
-        {'member_id': current_user.member_id}, fetch_one=True)[0]
+        {'member_id': member_id}, fetch_one=True
+    )[0]
+    return ceil(total_payments / per_page)
 
-    total_pages = ceil(total_payments / per_page)
 
-    payments = user_service.execute_db_query(
+def get_sorted_payments(params):
+    return params['user_service'].execute_db_query(
         "SELECT * FROM GetSortedPayments(:member_id, :sort_column, :sort_order) LIMIT :limit OFFSET :offset",
         {
-            'member_id': current_user.member_id,
-            'sort_column': sort_column,
-            'sort_order': sort_order,
-            'limit': per_page,
-            'offset': offset
-        }, fetch_all=True)
-
-    return render_template('payments.html', form=form, payments=payments, current_page=page, total_pages=total_pages)
+            'member_id': params['member_id'],
+            'sort_column': params['sort_column'],
+            'sort_order': params['sort_order'],
+            'limit': params['limit'],
+            'offset': params['offset']
+        }, fetch_all=True
+    )
 
 
 def logout_route():
